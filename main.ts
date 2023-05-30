ec54978139de4ec96dabcefc3fac4ebd8aea52df
Los-wapos-xd <131193171+Los-wapos-xd@users.noreply.github.com>
2023-05-30 11:43:12 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,20 @@
-input.onGesture(Gesture.LogoUp, function () {
-    radio.sendNumber(1)
-})
-input.onGesture(Gesture.ScreenUp, function () {
-    radio.sendNumber(132)
-})
-input.onGesture(Gesture.TiltRight, function () {
-    radio.sendNumber(3)
-})
-input.onGesture(Gesture.TiltLeft, function () {
-    radio.sendNumber(4)
-})
-input.onGesture(Gesture.LogoDown, function () {
-    radio.sendNumber(2)
-})
 radio.setGroup(132)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(8)
+    } else if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(9)
+    } else if (input.isGesture(Gesture.TiltLeft)) {
+        radio.sendNumber(4)
+    } else if (input.isGesture(Gesture.TiltRight)) {
+        radio.sendNumber(3)
+    } else if (input.isGesture(Gesture.LogoDown)) {
+        radio.sendNumber(2)
+    } else if (input.isGesture(Gesture.ScreenUp)) {
+        radio.sendNumber(132)
+    } else if (input.isGesture(Gesture.LogoUp)) {
+        radio.sendNumber(1)
+    } else {
+        radio.setGroup(132)
+    }
+})
